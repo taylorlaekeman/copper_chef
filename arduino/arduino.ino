a51ed3620c2ab4a1cf2a0797f8615325446f6b58
taylor <taylor.laekeman@gmail.com>
2017-07-19 14:33:25 -0400

    completed software for demonstration

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -5,18 +5,28 @@
 const int STEPS_PER_REVOLUTION = 800;
 
 Adafruit_MLX90614 thermometer = Adafruit_MLX90614();
-Stepper stepper(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
+Stepper linearSteppers(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
+Stepper rotationalStepper(STEPS_PER_REVOLUTION, 4, 5, 6, 7);
 
 void setup() {
   Serial.begin(9600);
   thermometer.begin();
-  stepper.setSpeed(75);
+  linearSteppers.setSpeed(75);
+  rotationalStepper.setSpeed(75);
+  
+  demonstrate();
 }
 
 void loop() {
-  //Serial.println(thermometer.readObjectTempF());
-  stepper.step(STEPS_PER_REVOLUTION * -30);
-  delay(1000);
-  stepper.step(STEPS_PER_REVOLUTION * 30);
-  delay(1000);
+  Serial.println(thermometer.readObjectTempC());
+  delay(500);
+}
+
+void demonstrate() {
+  linearSteppers.step(STEPS_PER_REVOLUTION * 10);
+  delay(100);
+  rotationalStepper.step(STEPS_PER_REVOLUTION * 0.5);
+  delay(100);
+  linearSteppers.step(STEPS_PER_REVOLUTION * -10);
+  delay(100);
 }
